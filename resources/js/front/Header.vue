140f040a946eea10a929c764cb119e3ddcfede25
Mustafijur10 <15-30734@student.aiub.edu>
2025-04-01 21:21:56 +0600

    main header design done

--- resources/js/front/Header.vue
+++ resources/js/front/Header.vue
@@ -7,7 +7,7 @@
                     <v-img src="/images/logo.png" max-width="240" class="header-logo ml-3"></v-img>
                 </router-link>
 
-                <v-text-field :loading="loading" append-inner-icon="mdi-magnify" density="compact" label="Search..."
+                <v-text-field :loading="loading" append-inner-icon="mdi-magnify" density="compact" label="Search Here..."
                     variant="outlined" hide-details single-line @click:append-inner="onClick" class="ml-5">
                 </v-text-field>
  
@@ -18,211 +18,45 @@
             </template>
         </v-menu>
 
-        <!-- <span class="mx-auto">
-
-            <v-menu open-on-hover >
-                <template v-slot:activator="{ props }">
-                    <router-link v-bind="props" to="/" class="ml-5 text-decoration-none text-link_text text-subtitle-2"> Camera </router-link>
-                </template>
-                <v-list class="rounded-xl mt-7 pa-2">
-                    <v-list-item class="mt-3 mb-2">
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/cv-maker.webp" width="120" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">Action Camera</span>
-                        </v-list-item-title> 
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/cover-letter-builder.webp" width="120" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">Camera Lenses</span>
-                        </v-list-item-title> 
-                       
-                        <VBtnDropdown class="font-weight-bold text-primary" block append-icon="mdi-arrow-right" >Make My CV Now</VBtnDropdown>             
-                    </v-list-item>
-                    
-                </v-list>
-            </v-menu>
-
-            <v-menu open-on-hover>
-                <template v-slot:activator="{ props }">
-                    <router-link v-bind="props" to="/" class="ml-5 text-decoration-none text-link_text text-subtitle-2"> Templates </router-link>
-                </template>
-                <v-list class="rounded-xl mt-7 pa-3">
-                    <v-list-item>
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/cv-templates.svg" width="25" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">CV Templates</span>
-                        </v-list-item-title>
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/cover-letter-template.svg" width="25" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">Cover Letter Templates</span>
-                        </v-list-item-title> 
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/resignation-letter.svg" width="25" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">Resignation Letter Templates</span>
-                        </v-list-item-title> 
-                                                         
-                    </v-list-item>
-                    <VBtnDropdown class="font-weight-bold text-primary" append-icon="mdi-arrow-right" block >Make My CV Now</VBtnDropdown>
-                    
-                </v-list>
-            </v-menu>
-
-            <v-menu open-on-hover>
-                <template v-slot:activator="{ props }">
-                    <router-link v-bind="props" to="/" class="ml-5 text-decoration-none text-link_text text-subtitle-2"> Examples </router-link>
-                </template>
-                <v-list class="rounded-xl mt-7 pa-3">
-                    <v-list-item>
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/cv-templates.svg" width="25" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">CV Examples</span>
-                        </v-list-item-title>
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/cover-letter-examples.svg" width="25" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">Cover Letter Examples</span>
-                        </v-list-item-title>
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/cv-personal-profile-examples.svg" width="25" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">CV Personal Profile Templates</span>
-                        </v-list-item-title>
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/cv-summary-examples.svg" width="25" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">CV Summary Templates</span>
-                        </v-list-item-title>
-                        
-                        
-                    </v-list-item>
-                    <VBtnDropdown class="font-weight-bold text-primary" append-icon="mdi-arrow-right" block>Make My CV Now</VBtnDropdown>            
-                    
-                </v-list>
-            </v-menu>
-
-            <v-menu open-on-hover>
-                <template v-slot:activator="{ props }">
-                    <router-link v-bind="props" to="/" class="ml-5 text-decoration-none text-link_text text-subtitle-2"> CV Writing </router-link>
-                </template>
-
-                <v-list class="rounded-xl mt-7 pa-3">
-                    <v-list-item>
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/how-to-write-a-cv.svg" width="25" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">How to Write a CV</span>
-                        </v-list-item-title>
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/how-to-lay-out-a-cv.svg" width="25" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">Best CV Format for 2024</span>
-                        </v-list-item-title>
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/top-skills-for-your-cv.svg" width="25" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">Top Skills for Your CV</span>
-                        </v-list-item-title>
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/23-personal-profile-examples.svg" width="25" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">CV Personal Statement Examples</span>
-                        </v-list-item-title>
-                        
-                        
-                    </v-list-item>
-                    <VBtnDropdown class="font-weight-bold text-primary" append-icon="mdi-arrow-right" block>Make My CV Now</VBtnDropdown>            
-                    
-                </v-list>
-            </v-menu>
-
-            <v-menu open-on-hover>
-                <template v-slot:activator="{ props }">
-                    <router-link v-bind="props" to="/" class="ml-5 text-decoration-none text-link_text text-subtitle-2"> Cover Letter Writting </router-link>
-                </template>
-
-                <v-list class="rounded-xl mt-7 pa-3">
-                    <v-list-item>
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/how-to-write-a-cover-letter.svg" width="25" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">How to Write a Cover Letter</span>
-                        </v-list-item-title>
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/how-to-start-a-cover-letter.svg" width="25" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">Starting a Cover Letter</span>
-                        </v-list-item-title>
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/how-to-format-a-cover-letter.svg" width="25" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">Cover Letter Formatting</span>
-                        </v-list-item-title>
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/what-is-a-cover-letter.svg" width="25" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">What Is a Cover Letter</span>
-                        </v-list-item-title>
-                        
-                             
-                    </v-list-item>
-                    <VBtnDropdown class="font-weight-bold text-primary" append-icon="mdi-arrow-right" block>Make My CV Now</VBtnDropdown>       
-                    
-                </v-list>
-            </v-menu>
-
-            <v-menu open-on-hover>
-                <template v-slot:activator="{ props }">
-                    <router-link v-bind="props" to="/" class="ml-5 text-decoration-none text-link_text text-subtitle-2"> Resources </router-link>
-                </template>
-
-                <v-list class="rounded-xl mt-7 pa-3">
-                    <v-list-item>
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/blog.svg" width="25" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">Blog</span>
-                        </v-list-item-title>
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/cv-help.svg" width="25" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">CV Help</span>
-                        </v-list-item-title>
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/cover-letter-help.svg" width="25" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">Cover Letter Help</span>
-                        </v-list-item-title>
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/Interview-help.svg" width="25" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">Interview Help</span>
-                        </v-list-item-title>
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/career-help.svg" width="25" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">Career Advice</span>
-                        </v-list-item-title>
-                        <v-list-item-title class="d-flex align-center mb-5"> 
-                            <img src="/images/faqs.svg" width="25" class="rounded-lg" />
-                            <span class="ml-3 text-subtitle-2">FAQ's</span>
-                        </v-list-item-title>
-                        
-                             
-                    </v-list-item>
-                    <VBtnDropdown class="font-weight-bold text-primary" append-icon="mdi-arrow-right" block>Make My CV Now</VBtnDropdown>      
-                    
-                </v-list>
-            </v-menu>
-           
-        </span> -->
-        
-        <!-- <v-divider class="ml-5 divider-height my-auto" vertical></v-divider> -->
-
         <v-spacer />
 
-        <!-- <v-menu bottom left offset-y origin="top right" transition="scale-transition">
-            <template v-slot:activator="{ attrs, on }">
-                <v-btn @click="themeSwitch" class="ml-2" min-width="0" color="primary" v-on="on">
-                    <v-icon>
-                        <template v-if="theme === 'light'" >mdi-weather-sunny</template>
-                        <template v-else>mdi-weather-night</template>
-                    </v-icon>
-                </v-btn>
-            </template>
-        </v-menu> -->
-          <v-menu open-on-hover>
+        <v-menu open-on-hover>
             <template v-slot:activator="{ props }">
-               <v-btn color="primary" v-bind="props" class="ma-0">
-                    <v-icon color="orange darken-2">mdi-account</v-icon>
+                <v-btn color="white" v-bind="props" class="ma-0 text-none text-left pl-1 pr-1 mr-2">
+                    <v-icon color="white darken-2 header-icon">mdi-account-outline</v-icon>
+                    <span class="pl-2"> 
+                        <small style="font-size: 10px; color:#696973;">Welcome</small>
+                        <br>
+                        <span style="font-size: 12px;">Sign In/ Register</span>
+                    </span>
+                    
                 </v-btn> 
-            </template>
-            <v-list>
-                <v-list-item prepend-icon="mdi-account" title="Account Settings" link @click="$router.push('/admin/account/settings')"></v-list-item>
-                <v-list-item prepend-icon="mdi-logout" @click="logout" title="Logout"></v-list-item>
-            </v-list>
+                
+            </template>            
+        </v-menu>
+        <v-menu open-on-hover>
+            <template v-slot:activator="{ props }">
+                <v-btn color="white" v-bind="props" class="ma-0 text-none text-left pl-1 pr-1 mr-2">                    
+                    <v-icon color="white darken-2 header-icon">mdi-bell-badge-outline</v-icon>
+                    <span class="pl-2"> 
+                        <small style="font-size: 10px; color:#696973;">0 Notifications</small>
+                        <br>
+                        <span style="font-size: 12px;">Check Notifications</span>
+                    </span>
+                </v-btn>
+            </template>            
+        </v-menu>
+        <v-menu open-on-hover>
+            <template v-slot:activator="{ props }">
+               <v-btn color="white" v-bind="props" class="ma-0 text-none text-left pl-1 pr-1">
+                    <span class="pr-2"> 
+                        <small style="font-size: 10px; color:#696973;">0 item(s)</small>
+                        <br>
+                        <span style="font-size: 12px;">Total: 0৳</span>
+                    </span>
+                    <v-icon color="white darken-2 header-icon">mdi-cart-variant</v-icon>
+                </v-btn> 
+            </template>            
         </v-menu>
     </v-toolbar>
 
